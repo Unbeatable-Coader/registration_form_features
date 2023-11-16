52a105c7bfa653ffdfb969e980c5c0a9fa3ad031
Harshit <harshdh55@gmail.com>
2023-11-16 12:12:15 +0530

    Improve the UI and some errors

--- app/controllers/password_reset_Controller.rb
+++ app/controllers/password_reset_Controller.rb
@@ -25,7 +25,7 @@ class PasswordResetController < ApplicationController
             render :edit
         end
     end
-
+ 
     private
     def password_params
         params.require(:user).permit(:password, :password_confirmation)
